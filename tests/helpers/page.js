595d3ff264c406eb233ec3ec1525677301a6dc2b
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-06-22 00:30:23 +0300

    reusable function

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const { chromium } = require('playwright')
+const sessionFactory = require('../factories/sessionFactory')
+const userFactory = require('../factories/userFactory')
 
 class CustomPage {
     constructor(page) {
@@ -19,12 +21,37 @@ class CustomPage {
             get: function (target, property) {
                 return (
                     target[property] ||
-                    target.page[property] ||
-                    target.browser[property] ||
-                    target.context[property]
+                    target.page[property]
                 );
             },
         });
+    }
+
+    async login() {
+        const user = await userFactory()
+        const { session, sig } = sessionFactory(user)
+
+        await this.context.addCookies([
+            {
+                name: 'session',
+                value: session,
+                domain: 'localhost',
+                path: '/', // Specify the URL path for the cookie
+            },
+            {
+                name: 'session.sig',
+                value: sig,
+                domain: 'localhost',
+                path: '/', // Specify the URL path for the cookie
+            },
+        ]);
+
+        await this.page.goto('http://localhost:3000/blogs');
+        await this.page.waitForTimeout(3000); // Add a 3-second delay
+    }
+
+    async getContentsOf(selector) {
+        return this.page.$eval(selector, el => el.innerHTML);
     }
 }
 
